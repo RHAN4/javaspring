1231dec50a0a858f3f30a97bb22b2e05cdc11e9a
Rhana <159554135+RHAN4@users.noreply.github.com>
2025-04-01 11:31:32 -0300

    Add files via upload

---
+++ validacao/validacao/src/main/java/com/senai/validacao/exception/EmailJaCadastradoException.java
@@ -0,0 +1,7 @@
+package com.senai.validacao.exception;
+
+public class EmailJaCadastradoException extends RuntimeException {
+  public EmailJaCadastradoException(String message) {
+    super(message);
+  }
+}
